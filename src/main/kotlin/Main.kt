742461a65483e3d8419896b1221eb9a033c55af7
omar <50619134+OMAR-3OOV@users.noreply.github.com>
2022-07-01 17:12:48 +0400

    Add Context commands system
    The context commands system for the user has been added to the bot.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,18 @@
 import commands.funCategory.MessengerEvent
 import commands.gamesCategory.RPCEvent
+import dev.minn.jda.ktx.interactions.commands.updateCommands
 import dev.minn.jda.ktx.jdabuilder.light
 import listeners.Events
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.sharding.DefaultShardManager
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder
 import net.dv8tion.jda.api.utils.AllowedMentions
+import net.dv8tion.jda.api.utils.ChunkingFilter
+import net.dv8tion.jda.api.utils.MemberCachePolicy
 import net.dv8tion.jda.api.utils.cache.CacheFlag
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -30,13 +37,20 @@ class Main {
 
             val jda = light(token, enableCoroutines = true, intents = gateways) {
                 enableCache(CacheFlag.ACTIVITY, CacheFlag.ONLINE_STATUS, CacheFlag.ROLE_TAGS, CacheFlag.MEMBER_OVERRIDES)
-                disableCache(EnumSet.of(CacheFlag.EMOTE, CacheFlag.VOICE_STATE))
-
+                disableCache(EnumSet.of(CacheFlag.EMOJI, CacheFlag.VOICE_STATE))
+                setMemberCachePolicy(MemberCachePolicy.ALL)
+                setChunkingFilter(ChunkingFilter.ALL)
                 addEventListeners(Events(Main), RPCEvent(), MessengerEvent())
             }.awaitReady()
+            
             AllowedMentions.setDefaultMentions(EnumSet.of(Message.MentionType.USER))
 
-            jda.upsertCommand("shutdown", "shutdown ${jda.selfUser.name}!w").queue()
+            jda.updateCommands()
+                .addCommands(
+                    Commands.user("Messenger"),
+                    Commands.user("Rock Paper Scissors")
+                ).queue()
+
             Logger().info("Bot has been built!")
         }
     }
